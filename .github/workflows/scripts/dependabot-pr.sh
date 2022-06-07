e60532e715b9e93bf10bafc874156c85d6893192
Alex Boten <aboten@lightstep.com>
2022-06-07 11:46:03 -0700

    Update dependabot-pr.sh

--- .github/workflows/scripts/dependabot-pr.sh
+++ .github/workflows/scripts/dependabot-pr.sh
@@ -3,7 +3,7 @@
 # TODO: discuss if each SIG should have its own or use a common bot
 # git config user.name opentelemetry-java-bot
 # git config user.email 97938252+opentelemetry-java-bot@users.noreply.github.com
-
+gh auth login
 
 PR_NAME=dependabot-prs/`date +'%Y-%m-%d-%H%M%S'`
 git checkout -b $PR_NAME
